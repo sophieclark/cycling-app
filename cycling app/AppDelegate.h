94b168077ea3ad6e5bfd6fc72107c42f3de8a72f
Sophie Clark <sophie.clark28@live.co.uk>
2018-08-17 12:35:42 +0100

    Initial Commit

---
+++ cycling app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cycling app
+//
+//  Created by Sophie Clark on 17/08/2018.
+//  Copyright © 2018 Sophie Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cycling app/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  cycling app
+//
+//  Created by Sophie Clark on 17/08/2018.
+//  Copyright © 2018 Sophie Clark. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
